6900edfa5a4f0b8283cb71f8cee64e5239a7b7b3
Vinay Samanthula <vinay.samanthula@avcitsolutions.com>
2026-04-02 17:09:36 +0530

    Client API created and it works

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lawyer_management_system.Data;
+using lawyer_management_system.Models;
+
+namespace lawyer_management_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ClientController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return Ok(client);
+        }
+    }
+}
